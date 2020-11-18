07d3632dee021cd7af0a38f52e037bea540777ef
Ravaniss <senechal.max06@gmail.com>
2020-11-18 22:43:47 +0100

    Demo9 - Start to 0

--- src/components/demos/Demo9.vue
+++ src/components/demos/Demo9.vue
@@ -42,76 +42,7 @@ export default {
       this.scene.add(gltf.scene);
       this.shirt = gltf.scene.children[1];
     },
-    createCanvasTexture () {
-      /* this one, doesn't appear ...
-      it was supposed to do the height & width dynamically using the font size */
-
-      const text = "Hello World !";
-      const fontSize = 16;
-      const font = 'arial';
-      const thatCanvas = document.createElement('canvas');
-      thatCanvas.style.marginTop = '200px';
-      thatCanvas.style.marginLeft = '300px';
-      const ctx = thatCanvas.getContext('2d');
-      ctx.font = 'Bold ' + fontSize + 'px ' + font;
-      ctx.fillStyle = 'orange';
-
-      thatCanvas.width = ctx.measureText(text).width;
-      thatCanvas.height = fontSize * 1.3;
-
-      console.log(thatCanvas.width, thatCanvas.height);
-
-      ctx.font = 'Bold ' + fontSize + 'px ' + font;
-      ctx.fillText(text, 0, thatCanvas.height / 1.3);
-
-      const newTexture = new THREE.CanvasTexture(thatCanvas);
-      newTexture.needsUpdate = true;
-
-      return newTexture;
-    },
-    createCanvasTextureW () {
-      const text = "Hello World !";
-      const thatCanvas = document.createElement('canvas');
-      thatCanvas.width = 1000; // testing purpose
-      thatCanvas.height = 1000;
-      const ctx = thatCanvas.getContext('2d');
-      ctx.font = "Bold 36px serif";
-      ctx.fillStyle = 'orange';
-      ctx.strokeStyle = 'red';
-      ctx.strokeRect(10, 10, 100, 100);
-      ctx.fillText(text, 400, 1100);
-      // how am I supposed to align my text at the middle of the shirt ???
-
-      const newTexture = new THREE.CanvasTexture(thatCanvas);
-      newTexture.needsUpdate = true;
-
-      return newTexture;
-    },
     incrustTextOnShirt () {
-      const material = new THREE.MeshBasicMaterial({
-        map: this.createCanvasTexture(),
-        transparent: false,
-        depthTest: false,
-        depthWrite: false,
-        polygonOffset: true,
-        polygonOffsetFactor: -14,
-        wireframe: true,
-      });
-
-      const position = new THREE.Vector3(0, 1, 0);
-      const orientation = new THREE.Euler();
-      const size = new THREE.Vector3(2, 2, 2);
-
-      const mesh = new THREE.Mesh(new DecalGeometry(this.shirt.children[0], position, orientation, size), material);
-      mesh.rotation.x = 1.5707964611537577;
-      mesh.rotation.y = 0;
-      mesh.rotation.z = -0;
-      mesh.scale.x = 1;
-      mesh.scale.y = 1;
-      mesh.scale.z = 1;
-      this.scene.add(mesh);
-    },
-    animate (time) {
     },
   },
   mounted () {
